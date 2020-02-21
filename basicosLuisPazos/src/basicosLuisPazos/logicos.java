c2bb817ca2c66953cfede0658108fa275eaa3f25
luismiguelpazosmoreno <lmpazos77@gmail.com>
2020-02-21 23:47:14 +0100

    inicial logicos

---
+++ basicosLuisPazos/src/basicosLuisPazos/logicos.java
@@ -0,0 +1,24 @@
+package basicosLuisPazos;
+
+public class logicos {
+
+	public static void main(String[] args) {
+        logicos();  // Llamada al método
+    }
+	
+		public static void logicos () {
+			// TODO Apéndice de método generado automáticamente
+			//Estos son los tipos básicos
+			boolean sol;
+			
+			//Ejemplos de asignación de valores a las variables de tipos básicos
+			sol = true;
+			
+			//Mostrar en pantalla el contenido de las variables de tipos básicos
+			if ( sol == true) {
+				System.out.println("Hoy hace sol, no está lloviendo");
+			}
+			
+		}
+	
+}
