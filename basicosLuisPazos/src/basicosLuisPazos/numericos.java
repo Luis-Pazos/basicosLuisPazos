ca54956e6145fa0a203e15d9b6a96932d34277de
luismiguelpazosmoreno <lmpazos77@gmail.com>
2020-02-21 23:43:07 +0100

    inicial numericos

---
+++ basicosLuisPazos/src/basicosLuisPazos/numericos.java
@@ -0,0 +1,33 @@
+package basicosLuisPazos;
+
+public class numericos {
+	
+	public static void main(String[] args) {
+		
+        numericos();  // Llamada al método
+    }
+
+	public static void numericos () {
+		// TODO Apéndice de método generado automáticamente
+	
+		//Estos son los tipos básicos
+		byte elMasPeque;
+		short chiquitin;
+		int entero;
+		long duplicaEntero;
+		float grande;
+		double doble;
+		
+		//Ejemplos de asignación a las variables de tipos básicos
+		entero = 10;
+		doble = 5.6;
+		
+		//Mostrar en pantalla el contenido de las variables de los tipos básicos
+		System.err.print("Valor entero ");
+		System.out.println(entero);
+		System.out.println("Valor doble ");
+		System.out.println(doble);
+	}
+
+}
+
